fd1258977668e5c2ac6977c507016f089c4c5566
Sofia Sousa <csofiamsousa@gmail.com>
2018-08-28 11:03:40 +0100

    tp26840 chore: Add installation script to gutenberg (3.6.2) and update webpack config script

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# go to node_modules
+mkdir -p node_modules
+cd node_modules
+
+# remove gutenberg if it already exists
+rm -rf gutenberg
+
+# clone gutenberg repo to node_modules
+git clone git@github.com:WordPress/gutenberg.git
+
+cd gutenberg
+
+# checkout to version we want
+git checkout tags/v3.6.2
+
+# remobe git references
+rm -rf .git
+
+# install gutenberg dependencies
+npm i
