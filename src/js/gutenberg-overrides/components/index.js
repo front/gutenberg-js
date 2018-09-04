a0e38cb02fd7249c23650d08187c2ce635b64565
Sofia Sousa <csofiamsousa@gmail.com>
2018-09-04 10:30:53 +0100

    tp26840 fix: Move components module back to overrides root folder

---
+++ src/js/gutenberg-overrides/components/index.js
@@ -0,0 +1,2 @@
+export { default as CategorySelect } from 'gutenberg/packages/components/build-module/query-controls/category-select.js';
+export * from 'gutenberg/components/index?source=node_modules';
