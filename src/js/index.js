4f6c132798d2d5ab629ead6733f5a23ad2754218
Sofia Sousa <csofiamsousa@gmail.com>
2018-09-20 10:09:57 +0100

    tp26840 feat: Export coreData module (and add list modules alphabetically)

--- src/js/index.js
+++ src/js/index.js
@@ -3,46 +3,53 @@
  */
 import './wp-init.js';
 import '../scss/style.scss';
-import '@wordpress/core-data';
 
 /**
  * WordPress dependencies
  */
-import * as i18n from '@wordpress/i18n';
+import * as blockLibrary from '@wordpress/block-library';
 import * as blocks from '@wordpress/blocks';
 import * as components from '@wordpress/components';
-import * as blockLibrary from '@wordpress/block-library';
-import * as element from '@wordpress/element';
+import * as coreData from '@wordpress/core-data';
 import * as data from '@wordpress/data';
 import * as editor from '@wordpress/editor';
 import * as editPost from '@wordpress/edit-post';
-import * as plugins from '@wordpress/plugins';
+import * as element from '@wordpress/element';
 import * as hooks from '@wordpress/hooks';
+import * as i18n from '@wordpress/i18n';
+import * as plugins from '@wordpress/plugins';
 
-// Adding modules to global wp
-window.wp.blocks = blocks;
-window.wp.i18n = i18n;
-window.wp.components = components;
+// and styles
+import 'gutenberg/block-library/editor.scss';
+import 'gutenberg/block-library/theme.scss';
+import 'gutenberg/block-library/style.scss';
+
+// Adding modules to wp global
 window.wp.blockLibrary = blockLibrary;
-window.wp.element = element;
+window.wp.blocks = blocks;
+window.wp.components = components;
+window.wp.coreData = coreData;
 window.wp.data = data;
 window.wp.editor = {
   ...window.wp.editor,
   ...editor,
 };
 window.wp.editPost = editPost;
-window.wp.plugins = plugins;
+window.wp.element = element;
 window.wp.hooks = hooks;
+window.wp.i18n = i18n;
+window.wp.plugins = plugins;
 
 export {
-  i18n,
+  blockLibrary,
   blocks,
   components,
-  blockLibrary,
-  element,
+  coreData,
   data,
   editor,
   editPost,
-  plugins,
+  element,
   hooks,
+  i18n,
+  plugins,
 };
